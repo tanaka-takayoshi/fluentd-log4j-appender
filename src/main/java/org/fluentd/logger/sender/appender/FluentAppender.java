6c84bb322c7447cdd26a26cb95a24276f749c8ba
xnopre <xnopre@gmail.com>
2016-05-26 12:58:43 +0200

    Add setter to enable appender configuration in log4j config file

--- src/main/java/org/fluentd/logger/sender/appender/FluentAppender.java
+++ src/main/java/org/fluentd/logger/sender/appender/FluentAppender.java
@@ -11,50 +11,62 @@ import org.fluentd.logger.FluentLogger;
 public class FluentAppender extends AppenderSkeleton {
 
 	private FluentLogger fluentLogger;
-	
-	protected String tag = "log4j-appender";
-	
-	protected String host = "localhost";
 
-	protected int port = 24224;
-	
-	protected String label = "label";
-	
+	private String tag = "log4j-appender";
+
+	private String host = "localhost";
+
+	private int port = 24224;
+
+	private String label = "label";
+
+	public void setTag(final String tag) {
+		this.tag = tag;
+	}
+
+	public void setHost(final String host) {
+		this.host = host;
+	}
+
+	public void setPort(final int port) {
+		this.port = port;
+	}
+
+	public void setLabel(final String label) {
+		this.label = label;
+	}
+
 	@Override
 	public void activateOptions() {
 		try {
 			fluentLogger = FluentLogger.getLogger(tag, host, port);
-		} catch (RuntimeException e) {
-//			addError("Cannot create FluentLogger.", e);
+		} catch (final RuntimeException e) {
+			// addError("Cannot create FluentLogger.", e);
 		}
 		super.activateOptions();
 	}
-	
+
 	@Override
 	public void close() {
-		// TODO Auto-generated method stub
 		try {
 			fluentLogger.flush();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
+		} catch (final IOException e) {
 			e.printStackTrace();
 		}
 	}
 
 	@Override
 	public boolean requiresLayout() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	protected void append(LoggingEvent event) {
-		Map<String, Object> messages = new HashMap<String, Object>();
+	protected void append(final LoggingEvent event) {
+		final Map<String, Object> messages = new HashMap<String, Object>();
 		messages.put("level", event.getLevel().toString());
 		messages.put("loggerName", event.getLoggerName());
 		messages.put("thread", event.getThreadName());
 		messages.put("message", event.getMessage().toString());
-		
 		fluentLogger.log(label, messages, event.getTimeStamp());
 	}
 
